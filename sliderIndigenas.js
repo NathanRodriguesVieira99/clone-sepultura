02e345585ed33291398ba328931d72ffd8ef995d
NathanRodrigues59 <nathanrodriguesvieira99@gmail.com>
2024-07-12 23:26:46 -0300

    biome

--- sliderIndigenas.js
+++ sliderIndigenas.js
@@ -1,18 +1,17 @@
-const slider = document.querySelector('.slider');
+const slider = document.querySelector(".slider");
 let slideIndex = 0;
 
-function nextSlide(){
-    slideIndex++;
-    if(slideIndex >= slider.children.length){
-        slideIndex = 0;
-    }
-    updateSlidePosition();
+function nextSlide() {
+	slideIndex++;
+	if (slideIndex >= slider.children.length) {
+		slideIndex = 0;
+	}
+	updateSlidePosition();
 }
 
-function updateSlidePosition(){
-    const slideWidth =slider.clientWidth;
-    slider.style.transform=`translateX(-${slideIndex * slideWidth}px)`;
-
+function updateSlidePosition() {
+	const slideWidth = slider.clientWidth;
+	slider.style.transform = `translateX(-${slideIndex * slideWidth}px)`;
 }
 
-setInterval(nextSlide,6000);
+setInterval(nextSlide, 6000);
